8df6b75132f1a12e0f75757f4a1ed82e5beacf8d
ErickMwirigi <erickmbae21@gmail.com>
2023-09-26 10:38:01 +0300

    Done Lab

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+dog = "cuddly"
+
+dict_map = {
+    "hungry": "Refilling food bowl.",
+    "thirsty": "Refilling water bowl.",
+    "playful": "Playing tug-of-war.",
+    "cuddly": "Snuggling.",
+}
+
+
+owner = dict_map.get(dog, "Reading newspaper.")
